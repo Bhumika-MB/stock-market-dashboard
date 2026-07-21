b369c5ea6f8dcc80e057ca7167b9ba0fa48a0c3e
Bhumika <bhumika.is22@bmsce.ac.in>
2026-07-21 20:46:40 +0530

    Handle yfinance rate limit

--- services/company_info.py
+++ services/company_info.py
@@ -1,16 +1,28 @@
 import yfinance as yf
 
+
 def get_company_info(ticker):
 
-    stock = yf.Ticker(ticker)
+    try:
+        stock = yf.Ticker(ticker)
+        info = stock.info
 
-    info = stock.info
+        return {
+            "name": info.get("longName", ticker),
+            "sector": info.get("sector", "N/A"),
+            "industry": info.get("industry", "N/A"),
+            "country": info.get("country", "N/A"),
+            "website": info.get("website", "N/A"),
+            "market_cap": info.get("marketCap")
+        }
 
-    return {
-        "name": info.get("longName", ticker),
-        "sector": info.get("sector", "N/A"),
-        "industry": info.get("industry", "N/A"),
-        "country": info.get("country", "N/A"),
-        "website": info.get("website", "N/A"),
-        "market_cap": info.get("marketCap", 0)
-    }
+    except Exception:
+
+        return {
+            "name": ticker,
+            "sector": "N/A",
+            "industry": "N/A",
+            "country": "N/A",
+            "website": "N/A",
+            "market_cap": None
+        }
